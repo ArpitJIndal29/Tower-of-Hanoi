f3e5df6e49767ab8b6ec823ae38048d383b3e0c6
Arpit Agarwal <45405421+ArpitJIndal29@users.noreply.github.com>
2019-06-18 21:45:53 +0530

    Add files via upload

--- saving.php
+++ saving.php
@@ -1,9 +1,9 @@
 <?php
 
-$n = intval($_GET['n']);
-$FromBar = intval($GET['fromBar']);
-$ToBar = intval(GET['toBar']);
-$DiscIndex = intval(GET['rect[upar]']);
+$n1 = intval($_GET['n1']);
+// $FromBar = intval($GET['fromBar']);
+// $ToBar = intval(GET['toBar']);
+// $DiscIndex = intval(GET['rect[upar]']);
 
 
 $conn = mysqli_connect('localhost','root','#DogLover69');
@@ -14,11 +14,11 @@ if(!$conn){
 mysqli_select_db($conn,"Hanoi_Tower");
 
 $sql = "INSERT INTO Data (No_of_plates)
-VALUES ($n)";
-mysqli_query($con,$sql);
+        VALUES ($n1)";
+mysqli_query($conn,$sql);
 
-$sql = "INSERT INTO Main (Disc_no, From_bar, To_bar)
-VALUES ($DiscIndex, $FromBar, $ToBar)";
-mysqli_close($conn);
+// $sql = "INSERT INTO Main (Disc_no, From_bar, To_bar)
+// VALUES ($DiscIndex, $FromBar, $ToBar)";
+// mysqli_close($conn);
 
 ?>
